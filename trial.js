e320960d8e4902c908f521d5548f592cad4ec068
arhaamwanii <arhamwani765@gmail.com>
2023-11-05 06:38:19 +0530

    .

--- trial.js
+++ trial.js
@@ -8,9 +8,15 @@ console.log(fast2);
 
 let user = {
     email: "one@gmail.com",
-    upi: "blah blah"
+    upi: "buser one upi"
 };
 
+let user2 = user;
+
+user2.upi = "user two upi";
+
+
 console.log(user.upi);
+console.log(user2.upi);
 
 //things inside a class can be accesed via the dotnotaition
