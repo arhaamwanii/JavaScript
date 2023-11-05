2b09628e2fa20b9a5a16a0e3bd159e8545a5cc21
arhaamwanii <arhamwani765@gmail.com>
2023-11-05 07:02:45 +0530

    .

--- trial.js
+++ trial.js
@@ -1,22 +1,34 @@
-let fast  = "you are a son your mother";
+//--
+   
+    //    let fast  = "you are a son of your mother";
 
-let fast2 = `${fast}! arent you` ;
-
-
-console.log(fast);
-console.log(fast2);
-
-let user = {
-    email: "one@gmail.com",
-    upi: "buser one upi"
-};
-
-let user2 = user;
-
-user2.upi = "user two upi";
+    //     let fast2 = `${fast}! arent you` ;
 
 
-console.log(user.upi);
-console.log(user2.upi);
+    //     console.log(fast);
+    //     console.log(fast2);
+
+    //     let user = {
+    //         email: "one@gmail.com",
+    //         upi: "buser one upi"
+    //     };
+
+    //     let user2 = user;
+
+    //     user2.upi = "user two upi";
+
+
+    //     console.log(user.upi);
+    //     console.log(user2.upi);
 
 //things inside a class can be accesed via the dotnotaition
+
+//we can also specify we the data type of an input in js
+
+ const score = 400;
+ 
+ console.log(score)
+
+ 
+
+
